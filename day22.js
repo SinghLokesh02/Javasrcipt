4b21baf509bc60faa7dde81934234a44acec1edf
Lokesh <lokeshsingh7695@gmail.com>
2023-08-31 15:09:50 +0530

    just thoery of ajax futher implementation left

---
+++ day22.js
@@ -0,0 +1,25 @@
+// Ajax Code with Harry
+
+/*
+Ajax
+
+-> It stands for asynchronous Javasrcipt and XML
+
+-> Ajax is not a programming language. Rather it's a set of existing technologies
+
+-> Ajax helps in fetching data asynchronously without interfering with the existing page
+
+-> No page reload/refresh
+
+-> Mordern websites use Json instead or XML for data transfer
+
+
+
+// Why use AJAX
+
+-> No page Reload/refresh
+-> Better user Experience
+-> Save network Bandwidth
+-> Very Interactive
+
+*/
