317691b41742a07a12d9efb42c9bc6feab9aaeb0
Lokesh <lokeshsingh7695@gmail.com>
2023-09-02 13:09:01 +0530

    fetch api

--- day22.js
+++ day22.js
@@ -23,3 +23,17 @@ Ajax
 -> Very Interactive
 
 */
+
+console.log("hello");
+fetch("https://api.lix-it.com/v1/li/linkedin/search/jobs?viewer_id=alfie-lambert&url=https://www.linkedin.com/jobs/search/?currentJobId=3436671233&keywords=lix")
+.then((response)=>{
+    console.log(response.status)
+    console.log(response.statusText)
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((err)=>{
+console.log(err);
+})
