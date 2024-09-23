3cc13ae7f9072132c86c9fb252513d6b897c85f2
Lokesh <lokeshsingh7695@gmail.com>
2024-09-23 07:25:13 +0530

    Important questions for Interviews

---
+++ day43.js
@@ -0,0 +1,13 @@
+// ***** Important Questions for Interviews
+
+// 1. How to check the Type of Array
+const arr = ["Lokesh","Diwakar","Aniket","Ritik"]
+console.log(typeof arr);
+console.log(Array.isArray(arr));
+
+
+// 2. Why typeof array is given as Object in Javascript
+
+
+
+
