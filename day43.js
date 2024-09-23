56b4d44f743867f47fa38bbebb4c1a8646f38cec
Lokesh <lokeshsingh7695@gmail.com>
2024-09-24 01:17:22 +0530

    Added Some more Questions

--- day43.js
+++ day43.js
@@ -9,5 +9,35 @@ console.log(Array.isArray(arr));
 // 2. Why typeof array is given as Object in Javascript
 
 
+// 3. Difference Between NodeList and HTMLCollections in Js
+
+
+// 4. Primitive Vs Reference Datatypes
+
+
+// 5. Ways to Declare arrays and objects in JavaScript
+
+
+// 6. Dot vs bracket notation syntax in JavaScript objects
+// Dot notation is used to access the properties of an object using the dot operator (.) and in dot notation will only work if the key is a single word (without any spaces), and it is a valid JavaScript identifier.
+// Bracket notation is used to access the properties of an object using the bracket notation ([]). In bracket notation, the key can be any string.
+
+
+// 7. What is GEC (Global Execution Context) in JavaScript
+// The Global Execution Context is the default or base execution context in which the JavaScript code runs. It is created before any other execution context and is responsible for executing the global code, which includes global variables, functions, and other statements.
+
+// 8. How Js Code Works
+// JavaScript code is executed in a single-threaded, synchronous manner. This means that the code is executed line by line, one statement at a time, in a sequential order. However, JavaScript also has asynchronous capabilities, which allow certain operations to be executed independently of the main execution flow.
+
+// And while Executing the Js Code the following steps are followed:
+// 1. The Code must be Error Free
+// 2. Scope of Variables are determined
+// 3. GEC is created
+// 4. Hoisting 
+// 5. Execution of Code line by line
+
+
+
+
 
 
